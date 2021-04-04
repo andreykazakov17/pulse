69d96f0a5bed72fb8fe9b8c7ff2c6d4ab8038387
Andrew <andreyalexandrovich.k@gmail.com>
2021-04-04 11:46:29 +0300

    added map, modal windows, various improvements

--- src/js/slider.js
+++ src/js/slider.js
@@ -35,5 +35,24 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
-      
+    
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn();
+    });
+
+    $('.button_mini').on('click', function() {
+        $('.overlay, #order').fadeIn();
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        });
+    });
+
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #final').fadeOut();
+    });
 });
